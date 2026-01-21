2b416e46791d3741af7555b7af25b7dbdd0bfe5b
lumixuser002 <51259213+playanose@users.noreply.github.com>
2026-01-21 15:54:18 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'xpro1-v5';
-const ASSETS = ['index.html', 'manifest.json', 'icon.png'];
+const CACHE_NAME = 'xpro-v1';
+const ASSETS = ['./index.html', './manifest.json'];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
